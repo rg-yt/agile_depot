8f921007e54cac733d46998a58fe80c0946c105d
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-03-18 18:11:54 -0400

    Added changes for iteration D3

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -1,10 +1,11 @@
-Module CurrentCart
+module CurrentCart
 
-private
+  private
 
-def set_cart
-  @cart = Cart.find(session[:cart_id])
-rescue ActiveRecord::RecordNotFound
-  @cart = Cart.create
-  session[:id_cart] = @card.id
+  def set_cart
+    @cart = Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    @cart = Cart.create
+    session[:cart_id] = @cart.id
+  end
 end
